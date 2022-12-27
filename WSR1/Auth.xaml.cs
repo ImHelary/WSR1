39774b67e8a8e555d650cb4b4a82912425073cc8
ImHelary <danilremeskov@yandex.ru>
2022-12-27 18:35:42 +0300

    Добавьте файлы проекта.

---
+++ WSR1/Auth.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace WSR1
+{
+    /// <summary>
+    /// Логика взаимодействия для Auth.xaml
+    /// </summary>
+    public partial class Auth : Window
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWin mainWin = new MainWin();
+            mainWin.Show();
+
+            this.Close();
+
+        }
+    }
+}
